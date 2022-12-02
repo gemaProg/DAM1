06482db674d51d1f55d7673560d1fcdcb85cba0f
gema <gema.garciafernandez@educa.madrid.org>
2022-12-02 12:34:25 +0100

    Cambio desde intellij

--- EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
+++ EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
@@ -16,6 +16,8 @@ public class EjemploGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World from Netbeans");
+        System.out.println("Hello desde intellij");
+
     }
     
 }
